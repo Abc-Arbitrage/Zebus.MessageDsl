464a7af90ef65076ebc35d713cb482280eabca1b
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-01-04 18:14:18 +0100

    Add integration test

---
+++ src/Abc.Zebus.MessageDsl.Generator.Integration/IntegrationTest.cs
@@ -0,0 +1,21 @@
+﻿#if INTEGRATION_TEST
+
+using System;
+
+namespace Abc.Zebus.MessageDsl.Generator.Integration
+{
+    public class IntegrationTest
+    {
+        static IntegrationTest()
+        {
+            GC.KeepAlive(typeof(SomeMessage));
+            GC.KeepAlive(typeof(InnerNamespace.InnerMessage));
+            GC.KeepAlive(typeof(Abc.Zebus.CustomNamespace.HasCustomNamespace));
+            GC.KeepAlive(typeof(Abc.Zebus.CustomExplicitNamespace.HasCustomExplicitNamespace));
+            GC.KeepAlive(typeof(ExplicitItems.A.ExplicitlyDefinedMessage));
+            GC.KeepAlive(typeof(ExplicitItems.B.ExplicitlyDefinedMessage));
+        }
+    }
+}
+
+#endif
