dd678bafa700c371d2f2710b92b1810911829512
Kuinox <n.vandeginste@abc-arbitrage.com>
2025-03-27 19:44:04 +0100

    Add a cli tool to generate proto or cs files from a .msg file. (#7)

---
+++ src/Abc.Zebus.MessageDsl.Tool.Tests/ToolTests.cs
@@ -0,0 +1,81 @@
+using NUnit.Framework;
+
+namespace Abc.Zebus.MessageDsl.Tool.Tests;
+
+[TestFixture]
+public class ToolTests
+{
+    private TextWriter _originalOut = null!;
+    private TextReader _originalIn = null!;
+    private StringWriter _output = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _originalOut = Console.Out;
+        _originalIn = Console.In;
+        _output = new StringWriter();
+        Console.SetOut(_output);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Console.SetOut(_originalOut);
+        Console.SetIn(_originalIn);
+    }
+
+    [Test]
+    public void should_generate_csharp_with_namespace()
+    {
+        Console.SetIn(new StringReader("TestMessage()"));
+
+        var exitCode = Program.Main(["--namespace", "TestNS", "--format", "CSharp"]);
+        Assert.That(exitCode, Is.EqualTo(0));
+        var outputString = _output.ToString();
+        Assert.That(outputString, Does.Contain("namespace TestNS"));
+        Assert.That(outputString, Does.Contain("class TestMessage"));
+    }
+
+    [Test]
+    public void should_generate_proto_output_with_empty_namespace_when_not_provided()
+    {
+        var tempFile = Path.GetTempFileName();
+        try
+        {
+            File.WriteAllText(tempFile, "TestMessage()");
+
+            var exitCode = Program.Main([tempFile, "--format", "Proto"]);
+            Assert.That(exitCode, Is.EqualTo(0));
+            var outputString = _output.ToString();
+            Assert.That(outputString.Contains("message TestMessage"));
+        }
+        finally
+        {
+            File.Delete(tempFile);
+        }
+    }
+
+    [Test]
+    public void should_error_on_file_not_found()
+    {
+        var exitCode = Program.Main(["invalid_file.msg"]);
+        Assert.That(exitCode, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void should_error_on_directory()
+    {
+        var aDirectory = Environment.CurrentDirectory;
+        var exitCode = Program.Main([aDirectory]);
+        Assert.That(exitCode, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void should_error_on_invalid_msg()
+    {
+        Console.SetIn(new StringReader("InvalidSyntax"));
+        var exitCode = Program.Main([]);
+        Assert.That(exitCode, Is.EqualTo(1));
+    }
+}
---
+++ src/Abc.Zebus.MessageDsl.Tool/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.CommandLine;
+using Abc.Zebus.MessageDsl.Ast;
+using Abc.Zebus.MessageDsl.Generator;
+
+namespace Abc.Zebus.MessageDsl.Tool;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var mainCommand = new RootCommand();
+        var path = new Argument<string?>(".msg file", "The .msg file to process.")
+        {
+            Arity = ArgumentArity.ZeroOrOne
+        };
+
+        var defaultNamespace = new Option<string?>(name: "--namespace", description: "The default namespace to use for the generated files", getDefaultValue: () => null);
+        var outputType = new Option<Format>(name: "--format", description: "The output format to generate (csharp or proto)", getDefaultValue: () => Format.Proto);
+
+        mainCommand.AddArgument(path);
+        mainCommand.AddOption(defaultNamespace);
+        mainCommand.AddOption(outputType);
+
+        mainCommand.SetHandler(
+            context =>
+            {
+                var parse = context.ParseResult;
+                var parsedPath = parse.GetValueForArgument(path);
+                var parsedNamespace = parse.GetValueForOption(defaultNamespace);
+                var parsedFormat = parse.GetValueForOption(outputType);
+                string? txt = null;
+                try
+                {
+                    if (parsedPath != null)
+                        txt = File.ReadAllText(parsedPath);
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.Error.WriteLine($"File {parsedPath} does not exist.");
+                    context.ExitCode = 1;
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Error reading file {parsedPath}: {ex.Message}");
+                    context.ExitCode = 1;
+                    return;
+                }
+
+                txt ??= Console.In.ReadToEnd();
+                var parsed = ParsedContracts.Parse(txt, parsedNamespace);
+                foreach (var error in parsed.Errors)
+                {
+                    Console.Error.WriteLine(error);
+                }
+
+                if (parsed.Errors.Count != 0)
+                {
+                    context.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var message in parsed.Messages)
+                {
+                    message.Options.Proto = true;
+                }
+
+                switch (parsedFormat)
+                {
+                    case Format.CSharp:
+                    {
+                        var cs = CSharpGenerator.Generate(parsed);
+                        Console.Write(cs);
+                        return;
+                    }
+                    case Format.Proto:
+                    {
+                        var proto = ProtoGenerator.Generate(parsed);
+                        Console.Write(proto);
+                        return;
+                    }
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        );
+
+        return mainCommand.Invoke(args);
+    }
+}
+
+internal enum Format
+{
+    Proto,
+    CSharp
+}
