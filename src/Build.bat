08539ca284fb0c04a006b1ee6a380375de22b2b2
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2026-04-17 14:02:37 +0200

    Update TFMs, use slnx, other minor stuff

--- src/Build.bat
+++ src/Build.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd "%~dp0"
-dotnet msbuild Abc.Zebus.MessageDsl.sln /v:m /t:Restore,Rebuild,Pack /nodeReuse:false /bl /p:Configuration=Release
+dotnet msbuild Abc.Zebus.MessageDsl.slnx /v:m /t:Restore,Rebuild,Pack /nodeReuse:false /bl /p:Configuration=Release
 popd
 pause
